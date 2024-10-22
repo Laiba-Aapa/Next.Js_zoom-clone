8e80a9409e30e2aaa2473cdbe9d371246f52a17c
Laiba Asif <laibasif36@gmail.com>
2024-10-22 22:22:52 +0500

    vercel issues solved

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -59,6 +59,7 @@ const MeetingTypeList = () => {
                 title: "Meeting Created",
             })
         } catch (error) {
+            console.log(error)
             toast({
                 title: "Failed to create meeting",
             })
